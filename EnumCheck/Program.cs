2b5d2be96ae698b0da58d0008756f251d1babdf9
veronika-ignatova <veronika.a.ignatova@gmail.com>
2023-09-22 16:41:37 +0300

    Add project files.

---
+++ EnumCheck/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using static Program.MyProgram;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Program
+{
+    class MyProgram
+    {
+        public enum MyEnum
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        public delegate void MyDelegate();
+
+        public static void Test(MyDelegate my)
+        {
+            var list = new List<long>();
+            Stopwatch watch = new Stopwatch();
+            for (int k = 0; k < 100; k++)
+            {
+                watch.Reset();
+                watch.Start();
+
+                for (int i = 0; i < 1000000; i++)
+                {
+                    my();
+                }
+                watch.Stop();
+                list.Add(watch.ElapsedTicks);
+            }
+            Console.WriteLine("RunTime " + list.Average());
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Write("Result of nameof: ");
+            Test(() => { var a = nameof(MyEnum) == "Monday"; });
+            Console.Write("Result of ToString ==: ");
+            Test(() => { var a = MyEnum.Monday.ToString() == "Monday"; });
+            Console.Write("Result of ToString Equals: ");
+            Test(() => { var a = MyEnum.Monday.ToString().Equals("Monday"); });
+            Console.Write("Result of ToString Equals Ignore Case: ");
+            Test(() => { var a = MyEnum.Monday.ToString().Equals("Monday", StringComparison.OrdinalIgnoreCase); });
+        }
+    }
+}
